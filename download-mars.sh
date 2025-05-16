27e60310226e24b2bddc7112f929d457d71bced7
JoaoManoelFontes <jmanufontesc@gmail.com>
2025-05-16 13:55:14 -0300

    feat: add download-mars script

---
+++ download-mars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+JAR_URL="https://github.com/dpetersanderson/MARS/releases/download/v.4.5.1/Mars4_5.jar"
+DEST_FILE="./mars.jar"
+
+echo "Baixando MARS IDE de: $JAR_URL"
+if curl -fL "$JAR_URL" -o "$DEST_FILE"; then
+    echo "✅ Download concluído com sucesso em: $DEST_FILE"
+else
+    echo "❌ Erro ao baixar o MARS. Verifique sua conexão ou a URL."
+    exit 1
+fi
